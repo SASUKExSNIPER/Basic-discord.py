129bd1bc8ee1a751943e64b4f0d514a3e5bfdc01
SASUKExSNIPER <72681530+SASUKExSNIPER@users.noreply.github.com>
2020-10-11 09:02:35 +0300

    Update sasuke.py

--- Episode1/sasuke.py
+++ Episode1/sasuke.py
@@ -8,7 +8,7 @@ async def on_ready():
 	global role_list
 	print('Logged in as: '+client.user.name)
 	print('Bot ID: '+client.user.id)
-	await client.change_presence(game=discord.Game(name='TZ!invites - List your invites')) 
+	await client.change_presence(game=discord.Game(name='sasuke.py')) 
 	print('------\n')
 	
 
